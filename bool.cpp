08be901659d066ef1000100be3b4c2ab5c20a0ed
Shivam <thisisshivamkumar8840@gmail.com>
2024-01-31 12:40:30 +0530

    Boolean c++

---
+++ bool.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a = 2/5;
+    // cout<< a << endl;
+int a=2;
+int b=3;
+
+bool first = (a>=b);
+cout<< first << endl;
+
+bool second = (a>b);
+cout<< second << endl;
+
+bool Third = (a<b);
+cout<< Third << endl;
+
+bool Fourth = (a>b);
+cout<< Fourth << endl;
+
+bool Fifth = (a>b);
+cout<< Fifth << endl;
+
+bool Sixth = (a>b);
+cout<< Sixth << endl;
+
+
+}
