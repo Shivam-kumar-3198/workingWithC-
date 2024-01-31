83ba5c4cd82af25043ff0c402169241e33035bdf
Shivam <thisisshivamkumar8840@gmail.com>
2024-01-31 12:38:43 +0530

    DataTypesAndVariables

---
+++ data types and variables.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=123;
+    cout<< a <<endl;
+    // char a = "v"; This Approach is completely wrong
+    // cout<< b <<endl;As we have Stored the value of a earlier
+    bool bl = true;
+    cout<< bl << endl;
+
+    float f = 1.23;
+    cout<<f<<endl;
+
+    int size = sizeof(a);
+    cout<<"Size of a is:"<<size <<endl;
+
+    // int size = sizeof(d);
+    // cout<<"Size of b is:"<<size<<endl;
+    
+
+
+}
