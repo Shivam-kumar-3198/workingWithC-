8827c80bad3a9fb5be190ec36f44cd3dab96220a
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-23 19:23:36 +0530

    d:\C++ BOOTCAMP\workingWithC-\Hollowsquare.cpp

---
+++ Hollowsquare.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     for(i=0; i<4; ++i)
+//     for(j=0; j<4; j++)
+//     {
+//         cout<< '*';
+//     }
+//     {
+//         else
+//         cout<<"";
+//     }
+ }
--- fullpyramid.cc
+++ fullpyramid.cc
@@ -1,3 +1,4 @@
+ /*
   #include<iostream>
   using namespace std;
   int main(){
@@ -14,4 +15,20 @@ for(int row=0; row<n; row= row+1){
     }
     cout << endl;
   }
+  }
+  */
+  #include<iostream>
+  using namespace std;
+  int main(){
+        int n;//int n;: This line declares a variable named n of type integer. It’s like reserving a memory slot to store a whole number.
+        cin >> n;//cin >> n;: Here, we’re using the cin object to read input from the user. Specifically, we’re reading an integer value and storing it in the variable n. So, if the user enters a number (e.g., 5), n will hold that value
+        for(int row=0;row<n; row=row+1){
+for(int row=0; row<n; row=row+1){
+  cout<<"";
+}
+for(int col=0;col<row+1; col=col+1){
+  cout<<"*";
+}
+cout<<endl; 
+        }
   }
